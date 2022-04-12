2b65ddbcf3051a038edcb4420347ce58e2a5f18c
Matt Withoos <mattwithoos@gmail.com>
2022-04-12 22:54:44 +1000

    Update valheim.go

--- src/backend/valheim/valheim.go
+++ src/backend/valheim/valheim.go
@@ -107,7 +107,7 @@ func (v *Valheim) Start(options StartOptions, callback func(error)) {
 		v.status = sStopped
 		return
 	}
-	err = os.Setenv("LD_PRELOAD", "./doorstop_libs/libdoorstop_x64.so:$LD_PRELOAD")
+	err = os.Setenv("LD_PRELOAD", "libdoorstop_x64.so:$LD_PRELOAD")
 	if err != nil {
 		v.status = sStopped
 		return
